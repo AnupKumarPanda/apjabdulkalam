43d1915f4f2059985fd6b70e36cf0f92c29b7bf1
anupkumarpanda <anupkumarpanda024@gmail.com>
2019-01-12 23:21:04 +0530

    addition program in a.java

--- src/stevejobs/a.java
+++ src/stevejobs/a.java
@@ -8,9 +8,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class a 
 {
+	//change  for git update
 	public static void main(String[] args) throws Exception
 	{
-		
+		int a =10;
+		int b =20;
+		int c =a+b;
+		 	
 	}
 		
 
